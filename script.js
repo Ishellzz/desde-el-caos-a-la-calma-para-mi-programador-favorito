cad56a215eb1ae690311f8140f38325da3394b45
Ishellzz <noelia.osoria23@gmail.com>
2026-06-03 16:40:17 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function actualizarContador() {
+
+    const fechaInicio = new Date("2026-02-06T00:00:00");
+    const ahora = new Date();
+
+    const diferencia = ahora - fechaInicio;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+    const horas = Math.floor(
+        (diferencia % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutos = Math.floor(
+        (diferencia % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const segundos = Math.floor(
+        (diferencia % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("contador").innerHTML =
+        `${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos`;
+}
+
+setInterval(actualizarContador, 1000);
+
+actualizarContador();
