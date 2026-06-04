cf39b2e9ad02de3a9bdfba900efd77cf810e5618
Ishellzz <noelia.osoria23@gmail.com>
2026-06-03 21:21:26 -0400

    script.js

--- script.js
+++ script.js
@@ -6,23 +6,21 @@ const ahora = new Date();
 
 const diferencia = ahora - fechaInicio;
 
-const dias = Math.floor(
-    diferencia / (1000 * 60 * 60 * 24)
-);
+const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
 
 const horas = Math.floor(
-    (diferencia % (1000 * 60 * 60 * 24))
-    / (1000 * 60 * 60)
+    (diferencia % (1000 * 60 * 60 * 24)) /
+    (1000 * 60 * 60)
 );
 
 const minutos = Math.floor(
-    (diferencia % (1000 * 60 * 60))
-    / (1000 * 60)
+    (diferencia % (1000 * 60 * 60)) /
+    (1000 * 60)
 );
 
 const segundos = Math.floor(
-    (diferencia % (1000 * 60))
-    / 1000
+    (diferencia % (1000 * 60)) /
+    1000
 );
 
 const contador = document.getElementById("contador");
